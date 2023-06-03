d37bfea05419a82545a7dd4d17cc340c9f8675fb
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2023-06-03 14:00:30 -0500

    Express migration

--- src/index.mjs
+++ src/index.mjs
@@ -1,21 +1,32 @@
-import http, { IncomingMessage, ServerResponse } from "http";
+import express from "express";
 import { showTaskController } from "./controllers.mjs";
+const app = express();
 const port = 3200;
+
+//Middlewares
+app.use(express.json());
 /**
  * 
- * @param {IncomingMessage} request 
- * @param {ServerResponse<IncomingMessage>} response 
+ * @param {express.Request} request 
+ * @param {express.Response} response 
  */
-const requestListener = async (request, response) => {
-    const data = await showTaskController();
-    const res = { message: "successfully retrieved", data }
-    response.writeHead(200, res.message)
-    response.write(JSON.stringify(res));
-    response.end();
+const showTasks = async (request, response) => {
+    try {
+        const data = await showTaskController();
+        const res = { message: "successfully retrieved", data }
+        response.status(200).json(res);
+        response.end();
+    } catch (error) {
+        console.log(error)
+        response.end();
+    }
 }
+app.get("/tasks", showTasks);
 
-const server = http.createServer(requestListener);
+app.use((request, response) => {
+    response.status(404).json({ message: "Resource not found", status: 404 })
+})
 
-server.listen(port, (request, response) => {
-    console.log(`Server listening on port ${port}`);
+export const server = app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
 })
